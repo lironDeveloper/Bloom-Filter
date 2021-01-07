0015c3f1dbc9e8af1181cd752c1c58306b2d3432
lironDeveloper <liron.ostrovsky@gmail.com>
2021-01-07 15:45:00 +0200

    FINITO

--- src/App.java
+++ src/App.java
@@ -1,6 +1,17 @@
 import java.util.Scanner;
 
+
+/*
+    Author: Liron Ostrovsky 7/1/2021
+    Course: Data Structures
+    Assignment: Data structure implementation
+*/
 public class App {
+    /*
+        Main - entry to the program
+        Algorigthm - Asks the user to enter the array bit size and the number of the hush functions to create the structure.
+                     Then, it creates a new structure, and calls the test function - explained in the Structure class.
+    */
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter structure size");
--- src/Structure.java
+++ src/Structure.java
@@ -4,11 +4,20 @@ import java.io.IOException;
 import java.util.Scanner;
 import com.google.common.hash.*;
 
+/*
+    Class - Structure
+    Properties - structureSize(integer - the size of the bitArray),
+                 hashFunctionsAmount(integer - the number of hash functions that create the structure),
+                 bitArray (BitSet - the bit array that holds the data which element is in the structure)
+*/
 public class Structure {
     private int structureSize;
     private int hashFunctionsAmount;
     private BitSet bitArray;
 
+    /*
+        Builder - sets the properties and calls the initStructure() that inits the structure from the input file.
+    */
     public Structure(int structureSize, int hashFunctionsAmount) {
         this.setStructureSize(structureSize);
         this.setHashFunctionsAmount(hashFunctionsAmount);
@@ -16,6 +25,16 @@ public class Structure {
         initStructure();
     }
 
+    /*
+        Input - null
+        Output - null
+        Algorithm - The function scannes an input file(Text file with numbers comma separated) and for each number,
+                    it creates k hash functions according to hashFunctionsAmount property, and k times hashes the number.
+                    For each result of each hash function, the function creates the absolute number of it and modulu by the bitArray size
+                    At the end we set to true the index we calculated in the bitArray.
+        Time complexity - O(n*k) while n is hash functions amount the program used to create the structure, and k is the number of the numbers
+                         inside the input file.
+    */
     private void initStructure(){
         try{
             File inputFile = new File("input.txt");
@@ -37,6 +56,15 @@ public class Structure {
         }
     }
 
+    /*
+        Input - null
+        Output - null
+        Algorithm - The function scannes a test file(Text file with numbers comma separated) and for each number there it tests wheteher the number
+                    is in the structure or not by calling isNumberInStructure() function. According to the result, it prints if the number is inside
+                    the structure.
+        Time complexity - O(n*k) while n is hash functions amount the program used to create the structure, and k is the number of the numbers
+                         inside the test file.
+    */
     public void testStructure(){
         try {
             File testFile = new File("testFile.txt");
@@ -58,22 +86,42 @@ public class Structure {
         }
     }
 
+    /*
+        Setter of structureSize property
+    */
     private void setStructureSize(int structureSize) {
         this.structureSize = structureSize;
     }
 
+    /*
+        Setter of hashFunctionsAmount property
+    */
     private void setHashFunctionsAmount(int hashFunctionsAmount) {
         this.hashFunctionsAmount = hashFunctionsAmount;
     }
 
+    /*
+        Getter of structureSize property
+    */
     private int getStructureSize(){
         return this.structureSize;
     }
 
+    /*
+        Getter of hashFunctionsAmount property
+    */
     private int getHashFucntionsAmount(){
         return this.hashFunctionsAmount;
     }
 
+    /*
+        Input - number(Integer)
+        Output - Whether the number is in the filet bloom(boolean)
+        Algorithm - Foreach hush function we used to initialize the structure, the function hashes the number,
+                    and checks if the right place in the bit array is 1.
+                    If all the cells are one - the number is probably in the structure, othorwise it is definitely not there.
+        Time complexity - O(n) while n is hash functions amount the program used to create the structure.
+    */
     private boolean isNumberInStructure(int num) {
         for(int seed = 1; seed <= this.getHashFucntionsAmount(); seed++){            
             HashFunction hf = Hashing.murmur3_32(seed);
